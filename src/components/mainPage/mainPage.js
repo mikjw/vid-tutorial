320f9673d9cca20d60f18372c6dd82a74414e6f0
Mike Walshe <mwalshe@deloitte.co.uk>
2021-05-19 17:04:54 +0100

    Add basic tutorials list

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,12 +1,41 @@
 import './mainPage.css';
 import { Component } from 'react';
+import axios from 'axios';
+import Tutorial from '../tutorial/tutorial'
 
 export default class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tutorialsList: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("https://lingumi-take-home-test-server.herokuapp.com/videoTutorials");
+    console.log(response.data);
+    this.setState({
+      tutorialsList: response.data,
+    })
+  }
+
+  listTutorials() {
+    return this.state.tutorialsList.map(tut => {
+      return <div className="tutorial">
+        <Tutorial key={tut.id} title={tut.videoTitle} teacher={tut.teacherName} tags={tut.tags} avgUserRating={tut.averageUserRating}/>
+      </div>
+    })
+  }
 
   render() {
     return (
-      <div className='homescreen'>
+      <div>
+      <div className="main-page-title">
         VID-TUTORIAL
+      </div>
+      <div className="tutorialsList">
+        {this.listTutorials()}
+      </div>
       </div>
     );
   }
--- src/components/tutorial/tutorial.js
+++ src/components/tutorial/tutorial.js
@@ -2,5 +2,45 @@ import './tutorial.css';
 import { Component } from 'react';
 
 export default class Tutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.id,
+      url: this.props.videoUrl,
+      title: this.props.title,
+      tags: this.props.tags, 
+      teacher: this.props.teacher,
+      avgUserRating: this.props.avgUserRating
+    }
+  }
 
+  listTags() {
+    return this.state.tags.map(tag => {
+      return <div className="tag">
+        {tag}
+      </div>
+    })
+  }
+
+  render() {
+    return (
+      <div className="tut-container">
+        <div className="tut-header">
+          {this.state.title}
+        </div>
+        <div className="tut-teacher-name">
+          Teacher: {this.state.teacher}
+        </div>
+        <div className="tut-tags-container">
+          Tags: {this.listTags()}
+        </div>
+        <div className="tut-rating">
+          Rating: {this.state.avgUserRating.toPrecision(2)}
+        </div>
+        <div>
+          -------------------------------
+        </div>
+      </div>
+    );
+  }
 }
