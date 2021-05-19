5beda1a7c99056dd5887fac0bae5691d49d635c2
Mike Walshe <mwalshe@deloitte.co.uk>
2021-05-19 15:38:53 +0100

    Remove unneeded files and set up structure

---
+++ src/components/mainPage/mainPage.js
@@ -0,0 +1,18 @@
+import './mainPage.css';
+import { Component } from 'react';
+
+export default class MainPage extends Component {
+
+  render() {
+    return (
+      <div className='homescreen'>
+        VID-TUTORIAL
+      </div>
+    );
+  }
+}
+
+
+
+
+
---
+++ src/components/tutorial/tutorial.js
@@ -0,0 +1,6 @@
+import './tutorial.css';
+import { Component } from 'react';
+
+export default class Tutorial extends Component {
+
+}
