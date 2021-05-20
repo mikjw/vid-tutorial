e118840b83611f9c977f0df8c92990be39c2c66a
Mike Walshe <mjwalshe@gmail.com>
2021-05-21 00:23:10 +0100

    Remove unneeded constructor

--- src/components/tutorial/tutorial.js
+++ src/components/tutorial/tutorial.js
@@ -2,20 +2,8 @@ import './tutorial.css';
 import { Component } from 'react';
 
 export default class Tutorial extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: this.props.id,
-      url: this.props.videoUrl,
-      title: this.props.title,
-      tags: this.props.tags, 
-      teacher: this.props.teacher,
-      avgUserRating: this.props.avgUserRating
-    }
-  }
-
   listTags() {
-    return this.state.tags.map(tag => {
+    return this.props.tags.map(tag => {
       return <div className="tag">
         {tag}
       </div>
@@ -26,19 +14,18 @@ export default class Tutorial extends Component {
     return (
       <div className="tut-container">
         <div className="tut-header">
-          {this.state.title}
+          {this.props.title}
         </div>
-        <div className="tut-teacher-name">
-          Teacher: {this.state.teacher}
+        <div>
+          <div className="tut-teacher-name">
+            Teacher: {this.props.teacher}
+          </div>
+          <div className="tut-rating">
+            Rating: {this.props.avgUserRating.toPrecision(2)}
+          </div>
         </div>
         <div className="tut-tags-container">
           Tags: {this.listTags()}
-        </div>
-        <div className="tut-rating">
-          Rating: {this.state.avgUserRating.toPrecision(2)}
-        </div>
-        <div>
-          -------------------------------
         </div>
       </div>
     );
